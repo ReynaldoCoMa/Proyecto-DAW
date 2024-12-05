05449da160704c68272abafd4d76247eb9816ee4
johanaguilar12 <A19204146@alumnos.uady.mx>
2024-12-05 02:41:08 -0600

    footer correcto en adoptar.php

--- Proyecto/html/adoptar.php
+++ Proyecto/html/adoptar.php
@@ -23,6 +23,7 @@ if ($result->num_rows > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vivero UADY - Adoptar Planta</title>
     <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> 
 </head>
 <body>
 
